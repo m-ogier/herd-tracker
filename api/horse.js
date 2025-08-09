05966f530310d4c4ad7f808da12b91582beaf509
m-ogier <mogier600@gmail.com>
2025-08-09 23:37:54 +0200

    Update horse.js

--- api/horse.js
+++ api/horse.js
@@ -1,8 +1,7 @@
 // api/horse.js
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   if (req.method === "GET") {
     return res.status(200).json({ ok: true, message: "API is live. Send POST to trigger." });
   }
   return res.status(405).json({ ok: false, error: "Method Not Allowed" });
-}
-
+};
